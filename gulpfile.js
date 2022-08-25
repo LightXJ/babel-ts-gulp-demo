be3fa745908f599d9428320cc1937fa883062029
LightXJ <2395461472@qq.com>
2022-08-25 20:26:16 +0800

    feat: 增加生成类型文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 const babel = require('gulp-babel');
+const ts = require('gulp-typescript');
 const babelConfig = require('./babel.config');
+
+const tsProject = ts.createProject('tsconfig.json');
  
 function compileJs(){
   return gulp.src('src/**/*.ts')
@@ -8,7 +11,14 @@ function compileJs(){
   .pipe(gulp.dest('dist'))
 }
 
+// 校验并生成类型声明文件
+function generateDts(){
+  var tsResult = gulp.src('src/**/*.ts')
+      .pipe(tsProject());
+  return tsResult.dts.pipe(gulp.dest('dist'))
+}
 
-const build = gulp.parallel(compileJs);
+
+const build = gulp.parallel(compileJs, generateDts);
 
 module.exports.build = build
